c7d6c96c845117880f09c5620966aedf736c763a
Aditya Vyavahare <vvaditya23@icloud.com>
2024-05-05 13:30:11 +0530

    Initial Commit

---
+++ Matrimony/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Matrimony
+//
+//  Created by Aditya Vyavahare on 05/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Matrimony/MatrimonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrimonyApp.swift
+//  Matrimony
+//
+//  Created by Aditya Vyavahare on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MatrimonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
